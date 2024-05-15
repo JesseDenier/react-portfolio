21099c66ca3c02ae8d0d1726e63d03d98a3dae0a
Jesse <jessecdenier@gmail.com>
2024-05-15 14:35:14 -0500

    About Me Page CSS Added.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,15 +4,17 @@ import portraitImg from "../assets/img/portrait.png";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>Born & raised Austinite trying to keep it weirdly under control.</p>
-      <img src={portraitImg} alt="Jesse Denier smiling" />
-      <p>
-        Having quit all social media, I've decided to create this site to share
-        updates about my life with family, old friends, and anyone else curious
-        enough to check if I've gone insane.
-      </p>
+    <div className="about-page">
+      <div className="about-content-container">
+        <h1>About Me</h1>
+        <p>Born & raised Austinite trying to keep it weirdly under control.</p>
+        <img src={portraitImg} alt="Jesse Denier smiling" />
+        <p>
+          Having quit all social media, I've decided to create this site to
+          share updates about my life with family, old friends, and anyone else
+          curious enough to check if I've gone insane.
+        </p>
+      </div>
     </div>
   );
 }
